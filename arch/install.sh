88a7319f48582ddf5743e54ae32bab7cea775912
Yusuke Kato <i.can.feel.gravity@gmail.com>
2018-01-24 17:48:07 +0900

    Update install.sh

--- arch/install.sh
+++ arch/install.sh
@@ -1,22 +1,27 @@
-#!/bin/bash
-umount $1
-umount $1
-wipefs -a $1 && sync
-dd if=/dev/zero of=$1 bs=1024 && sync
-lvremove $1
-pvremove $1
-parted -s -a optimal $1 -- mklabel msdos
-parted -s -a optimal $1 -- mkpart primary xfs 1 -1
-mkfs.xfs $1p1
-mount $1p1 /mnt
-mkdir /mnt/home
-mkdir /mnt/boot
-mkdir /mnt/home/kpango
-cp ./Xdefaults /mnt/home/kpango/.Xdefaults
-cp ./zshrc /mnt/home/kpango/.zshrc
-cp ./chroot.sh /mnt
-sudo cp ./mirrorlist /etc/pacman.d/mirrorlist
-pacstrap -i /mnt base base-devel archlinux-keyring net-tools wireless_tools wpa_supplicant wpa_actiond dialog cmake clang dmenu rxvt-unicode git neovim zsh tmux grub-bios wlc wayland sway i3status chromium openssh ntp ranger
+#!/bin/sh
+umount /dev/sda
+wipefs -a /dev/sda && sync
+shred -n 2 -z /dev/sda && sync
+lvremove /dev/sda
+pvremove /dev/sda
+parted /dev/sda \
+  -s mklabel gpt \
+  -s mkpart ESP fat32 0% 513MiB \
+  -s set 1 boot on
+  -s mkpart primary xfs 513MiB 100% \
+  -s p
+mkfs.vfat -cvIF32 /dev/sda1 
+mkfs.xfs /dev/sda2
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir -p /mnt/home/kpango
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/Xdefaults
+cp Xdefaults /mnt/home/kpango/.Xdefaults
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/chroot.sh
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/locale.gen
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/mirrorlist
+pacstrap -i /mnt base base-devel archlinux-keyring cmake clang dmenu rxvt-unicode git neovim zsh tmux grub efibootmgr wlc wayland sway i3status chromium openssh ntp ranger
 genfstab -U -p /mnt >> /mnt/etc/fstab
 cp ./mirrorlist /mnt/etc/pacman.d/mirrorlist
 cp ./locale.gen /mnt/etc/locale.gen
