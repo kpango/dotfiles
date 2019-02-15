c4be6c67a5664bed58777dbf34fb4bc35609a12e
kpango <i.can.feel.gravity@gmail.com>
2019-02-15 12:31:20 +0900

    fix

--- nix/nixpkg.nix
+++ nix/nixpkg.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
-let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
-in
+#let
+#  unstableTarball =
+#    fetchTarball
+#      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+#in
 {
   nixpkgs = {
     config = {
@@ -16,17 +16,17 @@ in
        enableGoogleTalkPlugin = true;
       };
       # Create an alias for the unstable channel
-      packageOverrides = pkgs: {
-        unstable = import unstableTarball {
-          config = config.nixpkgs.config;
-        };
-      };
+      # packageOverrides = pkgs: {
+      #   unstable = import unstableTarball {
+      #     config = config.nixpkgs.config;
+      #   };
+      # };
       pulseaudio = true;
     };
     overlays = [(self: super: {
-      bat = super.unstable.bat;
-      exa = super.unstable.exa;
-      chromium = super.unstable.chromium;
+      #bat = super.unstable.bat;
+      #exa = super.unstable.exa;
+      #chromium = super.unstable.chromium;
       neovim = super.neovim.override {
         withPython = true;
         withPython3 = true;
