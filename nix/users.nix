86ef745dfb2553c1a3091c01cf2ff1f645dc9538
kpango <i.can.feel.gravity@gmail.com>
2019-03-19 22:01:41 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- nix/users.nix
+++ nix/users.nix
@@ -1,27 +1,29 @@
 {
   users = {
     defaultUserShell = "/run/current-system/sw/bin/zsh";
-    users.kpango = {
-      description = "Yusuke Kato";
-      extraGroups = [
-        "adm"
-        "audio"
-        "cdrom"
-        "disk"
-        "docker"
-        "libvirtd"
-        "networkmanager"
-        "root"
-        "systemd-journal"
-        "users"
-        "video"
-        "wheel"
-      ];
-      createHome = true;
-      home = "/home/kpango";
-      isNormalUser = true;
-      uid = 1000;
-      shell = "/run/current-system/sw/bin/zsh";
+    users = {
+      kpango = {
+        description = "Yusuke Kato";
+        extraGroups = [
+          "adm"
+          "audio"
+          "cdrom"
+          "disk"
+          "docker"
+          "libvirtd"
+          "networkmanager"
+          "root"
+          "systemd-journal"
+          "users"
+          "video"
+          "wheel"
+        ];
+        createHome = true;
+        home = "/home/kpango";
+        isNormalUser = true;
+        uid = 1000;
+        shell = "/run/current-system/sw/bin/zsh";
+      };
     };
   };
 }
