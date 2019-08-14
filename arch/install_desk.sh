e708c5a60571a46d77fee0f6bdd06fba01e1f428
kpango <i.can.feel.gravity@gmail.com>
2019-08-14 19:07:45 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/install_desk.sh
+++ arch/install_desk.sh
@@ -1,24 +1,49 @@
 #!/bin/sh
 ip a
-umount /dev/nvme0n1
+echo "unmount volumes"
+lsblk
+umount /dev/nvme0n1 && sync
+umount /dev/nvme1n1 && sync
+echo "volumes unmounted"
+lsblk
+echo "wipe disks"
 wipefs -a /dev/nvme0n1 && sync
-shred -n 2 -z /dev/nvme0n1 && sync
-lvremove /dev/nvme0n1
-pvremove /dev/nvme0n1
-umount /dev/nvme1n1
 wipefs -a /dev/nvme1n1 && sync
+echo "disks wiped"
+lsblk
+echo "shred disks"
+shred -n 2 -z /dev/nvme0n1 && sync
 shred -n 2 -z /dev/nvme1n1 && sync
-lvremove /dev/nvme1n1
-pvremove /dev/nvme1n1
-mdadm --zero-superblock /dev/nvme0n1
-mdadm --zero-superblock /dev/nvme1n1
-parted -s -a optimal /dev/nvme0n1 -- mklabel gpt mkpart primary xfs 0 100% set 1 raid on
-parted -s -a optimal /dev/nvme1n1 -- mklabel gpt mkpart primary xfs 0 100% set 1 raid on
+echo "disks shredded"
+lsblk
+echo "lvremove"
+lvremove /dev/nvme0n1 && sync
+lvremove /dev/nvme1n1 && sync
+echo "lvremoved"
+lsblk
+echo "pvremove"
+pvremove /dev/nvme0n1 && sync
+pvremove /dev/nvme1n1 && sync
+echo "pvremoved"
+lsblk
+echo "mdadm clear"
+mdadm --zero-superblock /dev/nvme0n1 && sync
+mdadm --zero-superblock /dev/nvme1n1 && sync
+echo "mdadm cleared"
+lsblk
+echo "volume partitioning"
+parted -s -a optimal /dev/nvme0n1 -- mklabel gpt mkpart primary xfs 0 100% set 1 raid on && sync
+parted -s -a optimal /dev/nvme1n1 -- mklabel gpt mkpart primary xfs 0 100% set 1 raid on && sync
+echo "volume partitioned"
+lsblk
+echo "creating mdadm raid0"
 # parted -s -a optimal /dev/nvme0n1 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
 # parted -s -a optimal /dev/nvme0n1 -- mkpart primary xfs 513MiB 100%
 # parted -s -a optimal /dev/nvme1n1 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
 # parted -s -a optimal /dev/nvme1n1 -- mkpart primary xfs 513MiB 100%
 mdadm --create /dev/md0 --verbose --level=raid0 --chunk=256 --raid-devices=2 /dev/nvme0n1p1 /dev/nvme1n1p1
+echo "raid0 volume created"
+
 parted -s -a optimal /dev/md0 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
 parted -s -a optimal /dev/md0 -- mkpart primary xfs 513MiB 100%
 mkfs.vfat -cvIF32 /dev/md0p1
