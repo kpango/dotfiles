802fd46904e8911eb3030a4ccf4278cb0d4a0ca4
kpango <i.can.feel.gravity@gmail.com>
2019-08-14 19:08:06 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/install_desk.sh
+++ arch/install_desk.sh
@@ -44,25 +44,25 @@ echo "creating mdadm raid0"
 mdadm --create /dev/md0 --verbose --level=raid0 --chunk=256 --raid-devices=2 /dev/nvme0n1p1 /dev/nvme1n1p1
 echo "raid0 volume created"
 
-parted -s -a optimal /dev/md0 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
-parted -s -a optimal /dev/md0 -- mkpart primary xfs 513MiB 100%
-mkfs.vfat -cvIF32 /dev/md0p1
-mkfs.xfs /dev/md0p2
-mount /dev/md0p2 /mnt
-mkdir -p /mnt/boot
-mount /dev/md0p1 /mnt/boot
-mkdir -p /mnt/home/kpango
-rm -rf Xdefaults chroot.sh locale.gen mirrorlist
-wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/Xdefaults
-cp Xdefaults /mnt/home/kpango/.Xdefaults
-wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/chroot.sh
-wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/locale.gen
-wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/mirrorlist
-pacstrap -i /mnt base base-devel archlinux-keyring intel-ucode cmake ccache clang dmenu rxvt-unicode git neovim zsh tmux wlc wayland sway i3status openssh ntp ranger dosfstools
-genfstab -U -p /mnt >> /mnt/etc/fstab
-cp ./mirrorlist /mnt/etc/pacman.d/mirrorlist
-cp ./locale.gen /mnt/etc/locale.gen
-cp ./chroot.sh /mnt/chroot.sh
-echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
-mdadm --detail --scan >> /mnt/etc/mdadm.conf
-arch-chroot /mnt
+# parted -s -a optimal /dev/md0 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
+# parted -s -a optimal /dev/md0 -- mkpart primary xfs 513MiB 100%
+# mkfs.vfat -cvIF32 /dev/md0p1
+# mkfs.xfs /dev/md0p2
+# mount /dev/md0p2 /mnt
+# mkdir -p /mnt/boot
+# mount /dev/md0p1 /mnt/boot
+# mkdir -p /mnt/home/kpango
+# rm -rf Xdefaults chroot.sh locale.gen mirrorlist
+# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/Xdefaults
+# cp Xdefaults /mnt/home/kpango/.Xdefaults
+# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/chroot.sh
+# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/locale.gen
+# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/mirrorlist
+# pacstrap -i /mnt base base-devel archlinux-keyring intel-ucode cmake ccache clang dmenu rxvt-unicode git neovim zsh tmux wlc wayland sway i3status openssh ntp ranger dosfstools
+# genfstab -U -p /mnt >> /mnt/etc/fstab
+# cp ./mirrorlist /mnt/etc/pacman.d/mirrorlist
+# cp ./locale.gen /mnt/etc/locale.gen
+# cp ./chroot.sh /mnt/chroot.sh
+# echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
+# mdadm --detail --scan >> /mnt/etc/mdadm.conf
+# arch-chroot /mnt
