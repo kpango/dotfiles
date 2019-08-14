06802a1e3fbf1913a32539e7d962e8d6c49fc105
kpango <i.can.feel.gravity@gmail.com>
2019-08-14 22:57:03 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/install_desk.sh
+++ arch/install_desk.sh
@@ -11,10 +11,13 @@ wipefs -a /dev/nvme0n1 && sync
 wipefs -a /dev/nvme1n1 && sync
 echo "disks wiped"
 lsblk
-echo "shred disks"
+echo "shred nvme0n1"
 shred -n 2 -z /dev/nvme0n1 && sync
+echo "nvme0n1 shredded"
+lsblk
+echo "shred nvme1n1"
 shred -n 2 -z /dev/nvme1n1 && sync
-echo "disks shredded"
+echo "nvme1n1 shredded"
 lsblk
 echo "lvremove"
 lvremove /dev/nvme0n1 && sync
@@ -43,26 +46,42 @@ echo "creating mdadm raid0"
 # parted -s -a optimal /dev/nvme1n1 -- mkpart primary xfs 513MiB 100%
 mdadm --create /dev/md0 --verbose --level=raid0 --chunk=256 --raid-devices=2 /dev/nvme0n1p1 /dev/nvme1n1p1
 echo "raid0 volume created"
-
-# parted -s -a optimal /dev/md0 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
-# parted -s -a optimal /dev/md0 -- mkpart primary xfs 513MiB 100%
-# mkfs.vfat -cvIF32 /dev/md0p1
-# mkfs.xfs /dev/md0p2
-# mount /dev/md0p2 /mnt
-# mkdir -p /mnt/boot
-# mount /dev/md0p1 /mnt/boot
-# mkdir -p /mnt/home/kpango
-# rm -rf Xdefaults chroot.sh locale.gen mirrorlist
-# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/Xdefaults
-# cp Xdefaults /mnt/home/kpango/.Xdefaults
-# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/chroot.sh
-# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/locale.gen
-# wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/mirrorlist
-# pacstrap -i /mnt base base-devel archlinux-keyring intel-ucode cmake ccache clang dmenu rxvt-unicode git neovim zsh tmux wlc wayland sway i3status openssh ntp ranger dosfstools
-# genfstab -U -p /mnt >> /mnt/etc/fstab
-# cp ./mirrorlist /mnt/etc/pacman.d/mirrorlist
-# cp ./locale.gen /mnt/etc/locale.gen
-# cp ./chroot.sh /mnt/chroot.sh
-# echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
-# mdadm --detail --scan >> /mnt/etc/mdadm.conf
-# arch-chroot /mnt
+lsblk
+echo "raid partitioning"
+parted -s -a optimal /dev/md0 -- mklabel gpt mkpart ESP fat32 1 513MiB set 1 boot on
+parted -s -a optimal /dev/md0 -- mkpart primary xfs 513MiB 100%
+echo "raid partitioned"
+lsblk
+echo "raid formatting"
+mkfs.vfat -cvIF32 /dev/md0p1
+mkfs.xfs /dev/md0p2
+echo "raid formatted"
+lsblk
+echo "raid mount"
+mount /dev/md0p2 /mnt
+mkdir -p /mnt/boot
+mount /dev/md0p1 /mnt/boot
+mkdir -p /mnt/home/kpango
+echo "raid mounted"
+df -aT
+echo "download deps"
+rm -rf Xdefaults chroot.sh locale.gen mirrorlist
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/Xdefaults
+cp Xdefaults /mnt/home/kpango/.Xdefaults
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/chroot.sh
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/locale.gen
+wget https://raw.githubusercontent.com/kpango/dotfiles/master/arch/mirrorlist
+echo "deps downloaded"
+ls -la
+echo "start pacstrap"
+pacstrap -i /mnt base base-devel archlinux-keyring intel-ucode cmake ccache clang dmenu rxvt-unicode git neovim zsh tmux wlc wayland sway i3status ntp ranger
+echo "pacstrap finished"
+echo "copy or write config"
+genfstab -U -p /mnt >> /mnt/etc/fstab
+cp ./mirrorlist /mnt/etc/pacman.d/mirrorlist
+cp ./locale.gen /mnt/etc/locale.gen
+cp ./chroot.sh /mnt/chroot.sh
+echo LANG=en_US.UTF-8 > /mnt/etc/locale.conf
+mdadm --detail --scan >> /mnt/etc/mdadm.conf
+echo "copy or write config finished"
+arch-chroot /mnt
