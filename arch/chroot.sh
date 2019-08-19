7d5deba279014503b48f90474b6e018457ee12b2
kpango <i.can.feel.gravity@gmail.com>
2019-08-19 23:27:26 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/chroot.sh
+++ arch/chroot.sh
@@ -15,7 +15,8 @@ mkdir /boot/efi/EFI
 # grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub --boot-directory=/boot/efi/EFI --recheck --debug
 # grub-mkconfig -o /boot/efi/EFI/grub/grub.cfg
 bootctl --path=/boot install
-DEVICE_ID=$(lsblk -f | grep nvme1n1p2 | awk '{print $3}')
+DEVICE_ID=$(lsblk -f | grep p2 | awk '{print $3}')
+echo ${DEVICE_ID}
 cat <<EOF > /boot/loader/entries/arch.conf
 title   Arch Linux
 linux   /vmlinuz-linux
@@ -30,5 +31,3 @@ editor no
 EOF
 
 bootctl update
-
-exit
