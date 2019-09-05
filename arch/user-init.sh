2a294e8c60ed5bae222b1c421978912ee0e0cf88
kpango <i.can.feel.gravity@gmail.com>
2019-09-05 11:16:43 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/user-init.sh
+++ arch/user-init.sh
@@ -26,5 +26,6 @@ HACKGEN_VERSION="1.2.1"
 axel "https://github.com/yuru7/HackGen/releases/download/v${HACKGEN_VERSION}/HackGen_v${HACKGEN_VERSION}.zip"
 unzip HackGen_v${HACKGEN_VERSION}.zip
 sudo mv HackGen_v${HACKGEN_VERSION}/* /usr/share/fonts/TTF/
+sudo mv /usr/bin/google-chrome-unstable /usr/bin/chrome
 
 fc-cache -f -v
