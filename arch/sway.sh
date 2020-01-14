e419a0339016ec93ba2ec1297f6f65e5b5f71357
kpango <i.can.feel.gravity@gmail.com>
2020-01-14 10:24:57 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/sway.sh
+++ arch/sway.sh
@@ -1,10 +1,13 @@
 # /etc/profile
 rm -rf /tmp
+rm -rf $HOME/.cache
+rm -rf $HOME/.ccache
 
 export XKB_DEFAULT_OPTIONS=ctrl:nocaps
 
 setxkbmap -option ctrl:nocaps
 
+export SWAYSOCK=$(ls /run/user/*/sway-ipc.*.sock | head -n 1)
 if [[ -z $DISPLAY ]] && [[ $TTY = /dev/tty1 ]]; then
     exec sway
 fi
