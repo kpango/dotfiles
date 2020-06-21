ccc792f3e94244f07c07c4ad5613632aae4c5783
kpango <i.can.feel.gravity@gmail.com>
2020-06-22 02:46:18 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/user-init.sh
+++ arch/user-init.sh
@@ -5,9 +5,9 @@ makepkg -si
 cd -
 rm -r yay
 
-curl https://raw.githubusercontent.com/kpango/dotfiles/master/arch/aur.list -o /aur.list
+curl https://raw.githubusercontent.com/kpango/dotfiles/master/arch/aur.list -o aur.list
 pacman -Rs go
-yay -Syu - < /aur.list
+yay -Syu - < aur.list
 HACKGEN_VERSION="1.4.1"
 axel "https://github.com/yuru7/HackGen/releases/download/v${HACKGEN_VERSION}/HackGen_v${HACKGEN_VERSION}.zip"
 unzip HackGen_v${HACKGEN_VERSION}.zip
