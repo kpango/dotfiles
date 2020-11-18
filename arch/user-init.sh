d6da116f656f5265e2dc82ab428088cc9582b28c
kpango <i.can.feel.gravity@gmail.com>
2020-11-18 22:03:31 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/user-init.sh
+++ arch/user-init.sh
@@ -9,7 +9,7 @@ rm -r yay
 curl https://raw.githubusercontent.com/kpango/dotfiles/master/arch/aur.list -o aur.list
 pacman -Rs go
 yay -Syu - < aur.list
-HACKGEN_VERSION="$(curl --silent https://github.com/yuru7/releases/latest | sed 's#.*tag/\(.*\)\".*#\1#' | sed 's/v//g')"
+HACKGEN_VERSION="$(curl --silent https://github.com/yuru7/HackGen/releases/latest | sed 's#.*tag/\(.*\)\".*#\1#' | sed 's/v//g')"
 axel "https://github.com/yuru7/HackGen/releases/download/v${HACKGEN_VERSION}/HackGen_v${HACKGEN_VERSION}.zip"
 unzip HackGen_v${HACKGEN_VERSION}.zip
 sudo mv HackGen_v${HACKGEN_VERSION}/* /usr/share/fonts/TTF/
