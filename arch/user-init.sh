f9c1e29f80382f0e05eb276a2832b954d73e7e21
kpango <i.can.feel.gravity@gmail.com>
2020-11-18 18:06:08 +0900

    fix
    Signed-off-by: kpango <i.can.feel.gravity@gmail.com>

--- arch/user-init.sh
+++ arch/user-init.sh
@@ -9,11 +9,10 @@ rm -r yay
 curl https://raw.githubusercontent.com/kpango/dotfiles/master/arch/aur.list -o aur.list
 pacman -Rs go
 yay -Syu - < aur.list
-HACKGEN_VERSION="2.1.1"
+HACKGEN_VERSION="$(curl --silent https://github.com/yuru7/releases/latest | sed 's#.*tag/\(.*\)\".*#\1#' | sed 's/v//g')"
 axel "https://github.com/yuru7/HackGen/releases/download/v${HACKGEN_VERSION}/HackGen_v${HACKGEN_VERSION}.zip"
 unzip HackGen_v${HACKGEN_VERSION}.zip
 sudo mv HackGen_v${HACKGEN_VERSION}/* /usr/share/fonts/TTF/
 sudo cp /usr/bin/google-chrome-stable /usr/bin/chrome
 rm -rg HackGen*
-
 fc-cache -f -v
