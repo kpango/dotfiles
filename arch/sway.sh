049110d6329b179a95734b187bff6aca820f18fc
kpango <kpango@vdaas.org>
2021-09-07 11:23:07 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- arch/sway.sh
+++ arch/sway.sh
@@ -12,6 +12,16 @@ export XMODIFIERS="@im=fcitx5"
 export XKB_DEFAULT_OPTIONS=ctrl:nocaps
 export XKB_DEFAULT_LAYOUT=us
 
+export XDG_SESSION_TYPE=wayland
+export XDG_CURRENT_DESKTOP=sway
+export XDG_CURRENT_SESSION=sway
+export LIBSEAT_BACKEND=logind
+export QT_QPA_PLATFORM=wayland
+# export GDK_DPI_SCALE=1.5
+# export QT_SCALE_FACTOR=1.5
+# export MOZ_ENABLE_WAYLAND=1
+# export WINIT_UNIX_BACKEND=x11
+
 setxkbmap -option ctrl:nocaps
 
 ulimit -n 500000
@@ -23,7 +33,11 @@ if [[ -z $DISPLAY ]] && [[ $TTY = /dev/tty1 ]]; then
     XMODIFIERS="@im=fcitx5" \
     XKB_DEFAULT_OPTIONS=ctrl:nocaps \
     XKB_DEFAULT_LAYOUT=us \
+    XDG_SESSION_TYPE=wayland \
+    XDG_CURRENT_DESKTOP=sway \
+    XDG_CURRENT_SESSION=sway \
     LIBSEAT_BACKEND=logind \
+    QT_QPA_PLATFORM=wayland \
     WLR_DRM_DEVICES=/dev/dri/card1:/dev/dri/card0 \
     exec sway
 fi
