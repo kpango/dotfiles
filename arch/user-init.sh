3a11050a8c356901259be5527f06395a7445e696
kpango <kpango@vdaas.org>
2022-04-26 11:31:56 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- arch/user-init.sh
+++ arch/user-init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-pacman -Syu
+sudo pacman -Syu
+sudo chmod -R 777 /tmp
 git clone --depth 1 https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
@@ -15,15 +16,9 @@ elif [[ $family_name =~ "X1" ]]; then
 else
     curl https://raw.githubusercontent.com/kpango/dotfiles/master/arch/aur_desk.list -o aur.list
 fi
-pacman -Rs go
+sudo pacman -Rs go
 yay -Syu - < aur.list
-HACKGEN_VERSION="$(curl --silent https://github.com/yuru7/HackGen/releases/latest | sed 's#.*tag/\(.*\)\".*#\1#' | sed 's/v//g')"
-HACKGEN_FILENAME=HackGenNerd_v${HACKGEN_VERSION}
-axel "https://github.com/yuru7/HackGen/releases/download/v${HACKGEN_VERSION}/${HACKGEN_FILENAME}.zip"
-unzip ${HACKGEN_FILENAME}.zip
-sudo mv ${HACKGEN_FILENAME}/* /usr/share/fonts/TTF/
 sudo cp /usr/bin/google-chrome-stable /usr/bin/chrome
-rm -rg HackGen*
 fc-cache -f -v
 if [[ $family_name =~ "P1" ]]; then
     systemctl --user enable psd.service
