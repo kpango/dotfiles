761aa31a1a899fc87e57ec1951867d1cd24cbee4
kpango <kpango@vdaas.org>
2022-08-21 13:32:58 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- arch/chroot_p1.sh
+++ arch/chroot_p1.sh
@@ -28,6 +28,13 @@ Include = /etc/pacman.d/mirrorlist
 EOF
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 echo "blacklist iTCO_wdt" > /etc/modprobe.d/nowatchdog.conf
+cat <<EOF >>/etc/modprobe.d/iwlwifi.conf
+options iwlwifi 11n_disable=1 swcrypto=0 bt_coex_active=0 power_save=0
+options iwlmvm power_scheme=1
+options iwlwifi d0i3_disable=1
+options iwlwifi uapsd_disable=1
+options iwlwifi lar_disable=1
+EOF
 
 HOST="archpango"
 echo ${HOST} >>/etc/hostname
