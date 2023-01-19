b2190a1d08abf7c0e28b88a72b9d72eb41ed944e
kpango <kpango@vdaas.org>
2023-01-20 01:55:30 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- nvim/lua/plugins/lspsaga.lua
+++ nvim/lua/plugins/lspsaga.lua
@@ -4,11 +4,11 @@ if (not status) then
   return
 end
 
-saga.init_lsp_saga {
-  server_filetype_map = {
-    typescript = 'typescript'
-  }
-}
+-- saga.init_lsp_saga {
+--   server_filetype_map = {
+--     typescript = 'typescript'
+--   }
+-- }
 
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
