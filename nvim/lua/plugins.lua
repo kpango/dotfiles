852277142265015277f5c8cbf68bf156542e02d8
kpango <kpango@vdaas.org>
2023-06-23 03:18:41 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,6 +23,7 @@ end
 lazy.setup({
     {
         "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
         opts = function()
             local status, cmp = pcall(require, "cmp")
             if not status then
@@ -34,6 +35,25 @@ lazy.setup({
                 error("cmp_nvim_lsp is not installed install_path: " .. install_path .. " packpath: " .. vim.o.packpath)
                 return
             end
+            local status, lspkind = pcall(require, "lspkind")
+            if not status then
+                error("lspkind is not installed install_path: " .. install_path .. " packpath: " .. vim.o.packpath)
+                return
+            end
+            cmp.setup.cmdline("/", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = "buffer" }, --ソース類を設定
+                },
+            })
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                }, {
+                    { name = "cmdline" },
+                }),
+            })
             return {
                 snippet = {
                     expand = function(args)
@@ -42,29 +62,60 @@ lazy.setup({
                 },
                 sources = {
                     { name = "nvim_lsp" }, --ソース類を設定
+                    { name = "cmp_tabnine" },
                     { name = "vsnip" }, -- For vsnip users.
                     { name = "buffer" },
                     { name = "path" },
+                    {
+                        name = "look",
+                        keyword_length = 2,
+                        option = {
+                            convert_case = true,
+                            loud = true,
+                            --dict = '/usr/share/dict/words'
+                        },
+                    },
                 },
                 mapping = cmp.mapping.preset.insert {
+                    ["<Tab>"] = cmp.mapping.select_next_item(), --Ctrl+pで補完欄を一つ上に移動
                     ["<C-p>"] = cmp.mapping.select_prev_item(), --Ctrl+pで補完欄を一つ上に移動
                     ["<C-n>"] = cmp.mapping.select_next_item(), --Ctrl+nで補完欄を一つ下に移動
                     ["<C-l>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<C-y>"] = cmp.mapping.confirm { select = true }, --Ctrl+yで補完を選択確定
+                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 },
                 experimental = {
                     ghost_text = false,
                 },
                 capabilities = cmplsp.default_capabilities(),
+                formatting = {
+                    format = lspkind.cmp_format {
+                        mode = "symbol", -- show only symbol annotations
+                        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+                        before = function(entry, vim_item)
+                            return vim_item
+                        end,
+                    },
+                },
             }
         end,
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
             "hrsh7th/vim-vsnip",
+            "hrsh7th/cmp-vsnip",
             "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-path",
+            "onsails/lspkind.nvim",
         },
     },
+    { "tzachar/cmp-tabnine", build = "./install.sh", dependencies = "hrsh7th/nvim-cmp" },
     {
         "numToStr/Comment.nvim",
         config = true,
@@ -138,12 +189,79 @@ lazy.setup({
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        dependencies = "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+	    {"navarasu/onedark.nvim", config = true, opts = { style = "darker"}},
+        },
+        opts = {
+            sync_install = false,
+            highlight = {
+                enable = true,
+                disable = {},
+            },
+            indent = {
+                enable = true,
+                disable = {},
+            },
+            ensure_installed = {
+                "bash",
+                "c",
+                "cmake",
+                "cpp",
+                "css",
+                "cuda",
+                "dart",
+                "dockerfile",
+                "gitignore",
+                "go",
+                "gomod",
+                "graphql",
+                "help",
+                "html",
+                "http",
+                "java",
+                "javascript",
+                "json",
+                "json5",
+                "julia",
+                "kotlin",
+                "llvm",
+                "lua",
+                "make",
+                "markdown",
+                "markdown_inline",
+                "meson",
+                "ninja",
+                "nix",
+                "proto",
+                "python",
+                "regex",
+                "rego",
+                "rust",
+                "sql",
+                "toml",
+                "typescript",
+                "v",
+                "vim",
+                "yaml",
+                "zig",
+            },
+            autotag = {
+                enable = true,
+            },
+        },
     },
     { "williamboman/mason.nvim", config = true, dependencies = "neovim/nvim-lspconfig" },
     {
         "williamboman/mason-lspconfig.nvim",
         opts = {
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
             ensure_installed = {
                 "gopls",
                 "sumneko_lua",
@@ -284,20 +402,72 @@ lazy.setup({
                             return fn.executable "cspell" > 0
                         end,
                     },
+                    null_ls.builtins.diagnostics.eslint_d.with {
+                        diagnostics_format = "[eslint] #{m}\n(#{c})",
+                    },
+                    null_ls.builtins.diagnostics.fish,
+                    null_ls.builtins.diagnostics.eslint,
+                    null_ls.builtins.completion.spell,
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.deno_fmt.with {
+                        condition = function(utils)
+                            return not (utils.has_file { ".prettierrc", ".prettierrc.js", "deno.json", "deno.jsonc" })
+                        end,
+                    },
+                    null_ls.builtins.formatting.prettier.with {
+                        condition = function(utils)
+                            return utils.has_file { ".prettierrc", ".prettierrc.js" }
+                        end,
+                        prefer_local = "node_modules/.bin",
+                    },
                 },
             }
         end,
     },
     { "nvim-tree/nvim-web-devicons" },
-    { "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons", config = true },
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = "nvim-tree/nvim-web-devicons",
+        config = true,
+        opts = {
+            options = {
+                mode = "tabs",
+                separator_style = "slant",
+                always_show_bufferline = false,
+                show_buffer_close_icons = false,
+                show_close_icon = false,
+                color_icons = true,
+            },
+            highlights = {
+                separator = {
+                    fg = "#073642",
+                    bg = "#002b36",
+                },
+                separator_selected = {
+                    fg = "#073642",
+                },
+                background = {
+                    fg = "#657b83",
+                    bg = "#002b36",
+                },
+                buffer_selected = {
+                    fg = "#fdf6e3",
+                },
+                fill = {
+                    bg = "#073642",
+                },
+            },
+        },
+    },
     {
         "nvim-lualine/lualine.nvim",
         opts = {
             options = {
                 icons_enabled = true,
                 theme = "gruvbox-material",
-                component_separators = { left = "|", right = "|" },
-                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
                 disabled_filetypes = { "NvimTree", "packer", "TelescopePrompt" },
                 always_divide_middle = true,
                 globalstatuses = true,
@@ -329,18 +499,42 @@ lazy.setup({
                         },
                     },
                 },
-                lualine_x = { "filetype" },
+                lualine_x = {
+                    {
+                        "diagnostics",
+                        sources = { "nvim_diagnostic" },
+                        symbols = {
+                            error = " ",
+                            warn = " ",
+                            info = " ",
+                            hint = " ",
+                        },
+                    },
+                    "encoding",
+                    "filetype",
+                },
                 lualine_y = {
                     { "diagnostics", source = { "nvim-lsp" } },
                     { "progress" },
-                    { "location" },
                 },
-                lualine_z = { "" },
+                lualine_z = { "location" },
             },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = { { "filename", file_status = true, path = 1 } },
+                lualine_c = {
+                    {
+                        "filename",
+                        path = 2,
+                        file_status = true,
+                        shorting_target = 40,
+                        symbols = {
+                            modified = " [+]",
+                            readonly = " [RO]",
+                            unnamed = "Untitled",
+                        },
+                    },
+                },
                 lualine_x = { "filetype" },
                 lualine_y = { "%p%%", "location" },
                 lualine_z = {},
@@ -365,3 +559,10 @@ lazy.setup({
 }, {
     root = pkg_path,
 })
+
+local status, onedark = pcall(require, "onedark")
+if not status then
+    error "onedark colorscheme is not installed"
+    return
+end
+onedark.load()
