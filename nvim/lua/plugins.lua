8520dea3bb4858acfa47f175c60610255ad80bcf
kpango <kpango@vdaas.org>
2023-06-27 03:12:47 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,6 +40,31 @@ safe_require("lazy").setup({
             local cmp = safe_require "cmp"
             local capabilities =
                 safe_require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local on_attach = function(client, bufnr)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+                -- Mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local bufopts = { noremap = true, silent = true, buffer = bufnr }
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+                vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+                vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+                vim.keymap.set("n", "<space>wl", function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, bufopts)
+                vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+                vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+                vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+                vim.keymap.set("n", "<space>f", function()
+                    vim.lsp.buf.format { async = true }
+                end, bufopts)
+            end
             cmp.setup.cmdline({ "/", "?" }, {
                 mapping = cmp.mapping.preset.cmdline(),
                 sources = {
@@ -125,6 +150,7 @@ safe_require("lazy").setup({
                 experimental = {
                     ghost_text = false,
                 },
+                on_attach = on_attach,
                 capabilities = capabilities,
                 formatting = {
                     format = safe_require("lspkind").cmp_format {
