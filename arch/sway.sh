255dc376d534d09e0ebf577ca50dba2dc05ab8e9
kpango <kpango@vdaas.org>
2023-11-30 14:06:41 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- arch/sway.sh
+++ arch/sway.sh
@@ -2,12 +2,28 @@
 sudo rm -rf /tmp/* \
     $HOME/.ccache
 
+export CLUTTER_BACKEND=wayland
+export DESKTOP_SESSION=sway
 export DefaultImModule=fcitx5
+export GBM_BACKEND=nvidia-drm
+export GDK_BACKEND=wayland
 export GTK_IM_MODULE=fcitx5
+export KITTY_ENABLE_WAYLAND=1
 export LIBSEAT_BACKEND=logind
+export LIBVA_DRIVER_NAME=nvidia
+export MOZ_ENABLE_WAYLAND=1
+export MOZ_USE_XINPUT2=1
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_IM_MODULE=fcitx5
+export QT_QPA_PLATFORM="wayland-egl;xcb"
 export QT_QPA_PLATFORM=wayland
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export SDL_IM_MODULE=fcitx5
+export SDL_VIDEODRIVER=wayland
+export WLR_DRM_DEVICES=/dev/dri/card1:/dev/dri/card0
+export WLR_DRM_NO_ATOMIC=1
+export WLR_NO_HARDWARE_CURSORS=1
+export WLR_RENDERER=vulkan
 export XDG_CURRENT_DESKTOP=sway
 export XDG_CURRENT_SESSION=sway
 export XDG_SESSION_DESKTOP=sway
@@ -15,10 +31,10 @@ export XDG_SESSION_TYPE=wayland
 export XKB_DEFAULT_LAYOUT=us
 export XKB_DEFAULT_OPTIONS=ctrl:nocaps
 export XMODIFIERS="@im=fcitx5"
-# export GDK_DPI_SCALE=1.5
-# export QT_SCALE_FACTOR=1.5
-# export MOZ_ENABLE_WAYLAND=1
-# export WINIT_UNIX_BACKEND=x11
+export XWAYLAND_NO_GLAMOR=1
+export __GLX_VENDOR_LIBRARY_NAME=nvidia
+export __GL_GSYNC_ALLOWED=0
+export __GL_VRR_ALLOWED=0
 
 setxkbmap -option ctrl:nocaps
 
