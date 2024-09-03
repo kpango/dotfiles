0067a06b936edef8969cc7a262e63665d8923ba1
kpango <kpango@vdaas.org>
2024-09-03 10:48:34 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- arch/chroot_p1.sh
+++ arch/chroot_p1.sh
@@ -31,11 +31,8 @@ blacklist iTCO_vendor_support
 blacklist iTCO_wdt
 EOF
 cat <<EOF >>/etc/modprobe.d/iwlwifi.conf
-options iwlwifi 11n_disable=1 swcrypto=0 bt_coex_active=0 power_save=0
-options iwlmvm power_scheme=1
-options iwlwifi d0i3_disable=1
-options iwlwifi uapsd_disable=1
-options iwlwifi lar_disable=1
+options iwlwifi 11n_disable=1 swcrypto=0 bt_coex_active=0 power_save=0 uapsd_disable=1 d0i3_disable=1 lar_disable=1
+options iwlmvm power_scheme=1 bt_coex_active=0
 EOF
 
 HOST="archpango"
