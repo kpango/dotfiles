b4270146817b7333d7170f581a652ce3c2015353
kpango <kpango@vdaas.org>
2025-02-13 10:51:12 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,23 +68,43 @@ require("lazy").setup({
 			provider = "openai",
 			openai = {
 				endpoint = "https://api.openai.com/v1",
-				model = "gpt-4o",
+				model = "gpt-o1-mini",
 				timeout = 30000,
 				temperature = 0,
 				max_tokens = 4096,
 			},
+			behaviour = {
+				auto_apply_diff_after_generation = true,
+			},
 		},
 		build = "make",
 		dependencies = {
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
 			"MunifTanjim/nui.nvim",
-			"echasnovski/mini.pick",
-			"nvim-telescope/telescope.nvim",
-			"hrsh7th/nvim-cmp",
-			"ibhagwan/fzf-lua",
-			"nvim-tree/nvim-web-devicons",
-			"zbirenbaum/copilot.lua",
+			"nvim-lua/plenary.nvim",
+			"stevearc/dressing.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						use_absolute_path = false,
+					},
+				},
+			},
 			{
 				"MeanderingProgrammer/render-markdown.nvim",
 				opts = {
@@ -94,6 +114,7 @@ require("lazy").setup({
 			},
 		},
 	},
+
 
 	------------------------------------------------------------------
 	-- Plugin: lsp-zero.nvim (LSP の設定)
