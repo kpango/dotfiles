816b0fd9bd08aeac125170d71ba849cd5a6951e1
kpango <kpango@vdaas.org>
2025-02-15 14:06:22 +0900

    fix
    Signed-off-by: kpango <kpango@vdaas.org>

--- nvim/lua/settings/env.lua
+++ nvim/lua/settings/env.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 function M.load_token_from_pass()
-  local anthropic_token = vim.fn.system("pass show ai/anthropic | tr -d '\n'")
-  vim.env.ANTHROPIC_API_KEY = anthropic_token
-  local groq_token = vim.fn.system("pass show ai/groq | tr -d '\n'")
-  vim.env.GROQ_API_KEY = groq_token
-  local open_ai_token = vim.fn.system("pass show ai/open_ai | tr -d '\n'")
-  vim.env.OPEN_AI_API_KEY = open_ai_token
+	local anthropic_token = vim.fn.system("pass show ai/anthropic | tr -d '\n'")
+	vim.env.ANTHROPIC_API_KEY = anthropic_token
+	local groq_token = vim.fn.system("pass show ai/groq | tr -d '\n'")
+	vim.env.GROQ_API_KEY = groq_token
+	local open_ai_token = vim.fn.system("pass show ai/open_ai | tr -d '\n'")
+	vim.env.OPEN_AI_API_KEY = open_ai_token
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    M.load_token_from_pass()
-  end,
+	callback = function()
+		M.load_token_from_pass()
+	end,
 })
 
 return M
